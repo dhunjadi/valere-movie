01c1099bc857c1207c308e506681c567071a57cb
Dario Hunjadi <dario.hunjadi@hotmail.com>
2023-09-04 11:56:55 +0200

    created home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,14 @@
+import {useNavigate} from 'react-router-dom';
+
 const HomePage = () => {
-    return <div>HomePage</div>;
+    const navigate = useNavigate();
+
+    return (
+        <div className="p-home">
+            <h1>Your streaming guide for movies, TV shows & sports</h1>
+            <button onClick={() => navigate('movies')}>Discover movies & TV shows</button>
+        </div>
+    );
 };
 
 export default HomePage;
